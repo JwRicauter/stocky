4318de9b4ea1797fc717e9ee3d5648242285ffeb
JwRicauter <jwricauter@gmail.com>
2023-04-17 20:12:51 +0200

    adapting chart component and creating api

--- src/services/api.ts
+++ src/services/api.ts
@@ -14,3 +14,27 @@ export const getSymbols = async (signal: AbortSignal)  => {
   
   return response;
 }
+
+export const getCandles = async (signal: AbortSignal, symbols: string[], from: string, to: string) => {
+
+  let promises = await symbols.map( async symbol => {
+    const uri : string = process.env.REACT_APP_API_URI + 
+    'stock/candle?symbol=' + symbol + '&resolution=D&from=' + from + '&to=' + to +
+    '&token=' + process.env.REACT_APP_FINHUB_SECRET;
+  
+    let response = await fetch(
+      uri, {
+      method: 'GET',
+      signal: signal
+    }).then(
+      res => res.json()
+    ).catch(
+      err => {return err}
+    )
+    return response;
+    
+
+  })
+
+  return Promise.all(promises);
+}
